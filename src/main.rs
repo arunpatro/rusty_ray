29d64274ba6c4c90301842338eba520f06c44d5c
Arun Patro <arun.kumar.patro@gmail.com>
2023-02-24 17:34:40 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+mod primitives;
+use nalgebra::Vector3;
+// use image_utils::save_as_png;
+// mod image_utils;
+
+
+fn render_sphere_orthographic() {
+    let sphere = primitives::Sphere::new(Vector3::new(0., 0., 0.), 1.);
+    let mut camera =
+        primitives::Camera::new(Vector3::new(-1., 1., 1.), Vector3::new(-1., 1., 1.), 100);
+    let light = primitives::Light::new(Vector3::new(0., 0., 1.));
+
+    for i in 0..camera.image.nrows() {
+        for j in 0..camera.image.ncols() {
+            let ray = camera.ray(i, j);
+            assert!(ray.direction.z == -1.);
+            let intersection = sphere.intersects(&ray);
+            match intersection {
+                Some(intersection) => {
+                    // if intersects, calculate color
+                    let normal = sphere.normal(&intersection);
+                    let light_vector = (light.position - intersection).normalize();
+
+                    let itensity = normal.dot(&light_vector);
+
+                    camera.image[(i, j)] = itensity;
+                    camera.alpha[(i, j)] = 1.;
+                    print!(" Intersected at ({}, {}, {})", intersection.x, intersection.y, intersection.z);
+                }
+                None => {
+                    // print!(" Failed to intersect");
+                }
+            }
+        }
+    }
+
+    // save_as_png(camera, "sphere_orthographic.png");
+}
+
+fn main() {
+    println!("Hello, world!");
+    render_sphere_orthographic();
+}
---
+++ src/primitives.rs
@@ -0,0 +1,87 @@
+use nalgebra::{DMatrix, Vector3};
+
+pub struct Camera {
+    pub position: Vector3<f32>,
+    screen_origin: Vector3<f32>,
+    pub image: DMatrix<f32>,
+    pub alpha: DMatrix<f32>,
+}
+
+impl Camera {
+    pub fn new(position: Vector3<f32>, origin: Vector3<f32>, image_size: usize) -> Self {
+        Self {
+            position: position,
+            screen_origin: origin,
+            image: DMatrix::zeros(image_size, image_size),
+            alpha: DMatrix::zeros(image_size, image_size),
+        }
+    }
+
+    pub fn ray(&self, i: usize, j: usize) -> Ray {
+        let direction = self.screen_origin
+            + Vector3::new(
+                i as f32 * 2. / self.image.nrows() as f32,
+                j as f32 * 2. / self.image.ncols() as f32,
+                0., // z is always 0
+            )
+            - self.position;
+        let direction = direction.normalize();
+        let direction = Vector3::new(0., 0., -1.);
+        Ray::new(self.position, direction)
+    }
+}
+
+pub struct Light {
+    pub position: Vector3<f32>,
+}
+
+impl Light {
+    pub fn new(position: Vector3<f32>) -> Self {
+        Self { position }
+    }
+
+}
+
+pub struct Sphere {
+    center: Vector3<f32>,
+    radius: f32,
+}
+
+pub struct Ray {
+    pub origin: Vector3<f32>,
+    pub direction: Vector3<f32>,
+}
+
+impl Ray {
+    pub fn new(origin: Vector3<f32>, direction: Vector3<f32>) -> Self {
+        Self { origin, direction }
+    }
+}
+
+impl Sphere {
+    pub fn new(center: Vector3<f32>, radius: f32) -> Self {
+        Self { center, radius }
+    }
+
+    pub fn intersects(&self, ray: &Ray) -> Option<Vector3<f32>> {
+        assert!(ray.direction.norm_squared() == 1.);
+        let a = ray.direction.norm_squared();
+        let b = 2. * ray.direction.dot(&(ray.origin - self.center));
+        let c = (ray.origin - self.center).norm_squared() - self.radius.powi(2);
+        let discriminant = b.powi(2) - 4. * a * c;
+
+        if discriminant < 0. {
+            return None;
+        } else {
+            let t1 = (-b + discriminant.sqrt()) / (2. * a);
+            let t2 = (-b - discriminant.sqrt()) / (2. * a);
+            let t = if t1 < t2 { t1 } else { t2 };
+            return Some(ray.origin + t * ray.direction);
+        }
+    }
+
+    pub fn normal(&self, point: &Vector3<f32>) -> Vector3<f32> {
+        let normal = (point - self.center).normalize();
+        return normal;
+    }
+}
